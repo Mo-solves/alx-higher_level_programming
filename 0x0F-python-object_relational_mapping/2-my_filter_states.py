54d5ff2686038fd2a223f40e1cd29c04dc9fbf83
Mohamed Abdi <mosolves147@gmail.com>
2022-09-11 19:11:13 +0300

    Fixed error

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
-"""takes in an argument and displays all values in the
-states table of hbtn_0e_0_usa where name matches the argument.
 """
-if __name__ == "__main__":
-    import MySQLdb
-    import sys
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        charset="utf8")
-    cur = conn.cursor()
-    cur.execute(
-        "SELECT * FROM states WHERE name LIKE '%%{userinput}%%' \
-ORDER BY id ASC".format(
-            userinput=sys.argv[4]))
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
+script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument
+"""
+from sys import argv
+import MySQLdb
+
+
+if __name__ == '__main__':
+    user, password, database, state = argv[1], argv[2], argv[3], argv[4]
+    db = MySQLdb.connect(host="localhost",
+                         user=user, passwd=password, db=database)
+    db = db.cursor()
+    db.execute("""SELECT * FROM states
+    WHERE name LIKE BINARY '{}' ORDER BY id"""
+               .format(state))
+    r = db.fetchall()
+    for i in r:
+        print(i)
